aa5304b9b6aaf78f7367118179c319e36eb9351e
Dora Tadic <dtadic1@pmfst.hr>
2020-10-25 16:41:38 +0100

    Promjene

--- src/App.js
+++ src/App.js
@@ -77,7 +77,7 @@ const promjenaFilter = (e) => { //filter
   return(
     <div>
       <div>
-        <h1>Adresar - novo</h1>
+        <h1>Adresar - Novo</h1>
         <ul>
           {zaIspis.map(o => 
           <Osoba 
